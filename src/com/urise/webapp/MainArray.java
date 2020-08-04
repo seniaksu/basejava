c471d6fdcb2814fa57e9d2350742d56119a55fe5
seniaksu <pushkarevakg@alfastrah.ru>
2020-08-04 13:16:14 +0700

    HW 6

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -20,13 +20,13 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save fullName | update uuid and fullName | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | save fullName | update uuid and fullName | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 3) {
                 System.out.println("Неверная команда.");
                 continue;
             }
-            String parametr = "name";
+            String parametr = null;
             if (params.length == 1) {
                 parametr = params[1].intern();
             }
