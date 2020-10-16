28bfa07cb49eb200cc0540b494f4ada115775bf1
seniaksu <pushkarevakg@alfastrah.ru>
2020-10-16 22:33:21 +0700

    HW 11 error correction

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -9,41 +9,39 @@ public class MainConcurrency {
     private static final Object LOCK = new Object();
 
     public static void main(String[] args) throws InterruptedException {
-        String lock1="lock1";
-        String lock2="lock2";
+        String lock1 = "lock1";
+        String lock2 = "lock2";
 
-        System.out.println(Thread.currentThread().getName());
+        printData("Thread", "");
 
-        Thread thread0 = new Thread() {
-            @Override
-            public void run() {
-                System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
-            }
-        };
+        Thread thread0 = new Thread(() -> {
+            printData("Thread", "");
+            throw new IllegalStateException();
+        });
         thread0.start();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+                printData("Thread", "");
             }
+
             private void inc() {
                 synchronized (this) {
-                // counter++;
+                    // counter++;
                 }
             }
         }).start();
 
-        System.out.println(thread0.getState());
+        printData("Thread", "");
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
         List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
             Thread thread = new Thread(() -> {
-            for (int j = 0; j < 100; j++) {
-                mainConcurrency.inc();
-            }
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
             });
             thread.start();
             threads.add(thread);
@@ -70,24 +68,27 @@ public class MainConcurrency {
 //        }
     }
 
-    private static void deadlock (Object lock1, Object lock2){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Wait" + lock1);
-                synchronized (lock1) {
-                    System.out.println("Catch" + lock1);
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Wait" + lock2);
-                    synchronized (lock2) {
-                        System.out.println("Catch" + lock2);
-                    }
+    private static void deadlock(Object lock1, Object lock2) {
+        new Thread(() -> {
+            printData("Wait", lock1);
+            synchronized (lock1) {
+                printData("Catch", lock1);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                printData("Wait", lock2);
+                synchronized (lock2) {
+                    printData("Catch", lock2);
                 }
             }
         }).start();
+    }
+
+    private static void printData(String message, Object lock) {
+        System.out.println(message + " " + lock + ": "
+                + Thread.currentThread().getName() + ", "
+                + Thread.currentThread().getState());
     }
 }
